d509c8f17993fdacbbc75562667b1df6daf1731f
Mas-Pey <konamexxxpes@gmail.com>
2025-10-03 23:35:45 +0700

    feat: Implement CRUD operations with sqlite database

--- src/features/employee.ts
+++ src/features/employee.ts
@@ -1,6 +1,6 @@
 import type { FastifyPluginAsync, FastifyReply, FastifyRequest } from "fastify"
 import fp from 'fastify-plugin'
-
+import db from './db.ts'
 
 /**
  * TYPES 
@@ -20,47 +20,75 @@ interface EmployeeBody {
  */
 const employeeRoutes: FastifyPluginAsync = async (fastify) => {
     fastify.get('/', async (request, reply) => {
-        return { message: 'Hello, Fastify with TypeScript!' }
+        return { message: 'Halo Karyawan :*' }
     })
 
     fastify.get<{ Params: EmployeeParams }>('/employee/:id',
         async (request, reply) => {
-            const { id } = request.params
+            const stmt = db.prepare("SELECT * FROM employees WHERE id = ?").get(request.params.id)
+            if (!stmt) {
+                return reply.status(404).send({
+                    message: "Employee not found"
+                })
+            }
             return {
-                userId: id,
-                message: `Employee ID: ${id}`
+                success: true, 
+                employee: stmt
             }
         }
     )
 
     fastify.post<{ Body: EmployeeBody }>('/employee',
         async (request, reply) => {
-            const { name } = request.body
+            if (typeof request.body.name !== "string") {
+                return reply.status(404).send({
+                    success: true,
+                    message: "Name must be a text"
+                })
+            }
+            const stmt = db.prepare("INSERT INTO employees (name) VALUES (?)").run(request.body.name)
             return {
                 success: true,
-                user: { name },
-                message: `Welcome ${name}`
+                message: `Employee '${request.body.name}' successfully added`,
+                employee: {
+                    id: stmt.lastInsertRowid,
+                    name: request.body.name
+                }
             }
         }
     )
 
     fastify.put<{ Params: EmployeeParams; Body: EmployeeBody }>('/employee/:id',
         async (request, reply) => {
-            const { id } = request.params
-            const { name } = request.body
+            const stmt = db.prepare("SELECT * FROM employees WHERE id = ?").get(request.params.id)
+            if (!stmt) {
+                return reply.status(404).send({
+                    message: "Employee not found"
+                })
+            }
+            db.prepare("UPDATE employees SET name = ? WHERE id = ?").run(request.body.name, request.params.id)
             return {
                 success: true,
-                message: `Update employee successfully`
+                employee: { 
+                    id: request.params.id, 
+                    name: request.body.name 
+                },
+                message: `Employee ID : '${request.params.id}' successfully updated`
             }
         }
     )
 
     fastify.delete<{ Params: EmployeeParams }>('/employee/:id',
-        async (request: FastifyRequest<{ Params: EmployeeParams }>, reply: FastifyReply) => {
-            const { id } = request.params
+        async (request, reply) => {
+            const stmt = db.prepare("DELETE FROM employees WHERE id = ?").run(request.params.id)
+            if (stmt.changes === 0) {
+                return reply.status(404).send({
+                    message: "Employee not found"
+                })
+            } 
             return {
-                userId: id,
-                message: `Delete employee succesfully`
+                success: true,
+                message: `Employee ID : '${request.params.id}' successfully deleted`
             }
         }
     )
