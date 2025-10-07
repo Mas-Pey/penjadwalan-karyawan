fdd752a04481a7bdc40d5f4cf136469d05832489
Mas-Pey <konamexxxpes@gmail.com>
2025-10-07 16:16:42 +0700

    test: add error handling tests for data validation

--- src/index.test.ts
+++ src/index.test.ts
@@ -14,9 +14,7 @@ test("can create employee", async (t: TestContext) => {
     const response = await app.inject({
         method: 'POST',
         url: '/employee',
-        payload: {
-            name: 'Newname-1'
-        }
+        payload: { name: 'Newname-1' }
     })
 
     t.assert.strictEqual(response.statusCode, 200, 'response 200')
@@ -24,6 +22,26 @@ test("can create employee", async (t: TestContext) => {
     t.assert.deepStrictEqual(json.employee.name, 'Newname-1')
     t.assert.ok(json.employee.id)
     employeeId = json.employee.id
+})
+
+test("return error (404) if name is not a string", async (t: TestContext) => {
+    t.plan(2)
+    const app = buildApp({
+        logger: false
+    })
+
+    const response = await app.inject({
+        method: 'POST',
+        url: '/employee',
+        payload: {
+            name: 2025
+        }
+    })
+
+    t.assert.strictEqual(response.statusCode, 404, 'response error')
+    t.assert.deepStrictEqual(response.json(), {
+        message: "Name must be a text"
+    })
 })
 
 test("can return employee info", async (t: TestContext) => {
@@ -60,7 +78,26 @@ test("can update employee data", async (t: TestContext) => {
     t.assert.deepStrictEqual(json.employee.name, 'Mulyadi')
 })
 
+test("return error (404) if employee not found when updating", async (t:TestContext) => {
+    t.plan(2)
+    const app = buildApp({
+        logger: false
+    })
+
+    const response = await app.inject({
+        method: 'PUT',
+        url: '/employee/-1',
+        payload: { name: 'Mulyadi' }
+    })
+
+    t.assert.strictEqual(response.statusCode, 404, 'response error')
+    t.assert.deepStrictEqual(response.json(), {
+        message: "Employee not found"
+    })
+})
+
 test("can delete employee", async (t: TestContext) => {
+    t.plan(4)
     const app = buildApp({
         logger: false
     })
@@ -81,4 +118,21 @@ test("can delete employee", async (t: TestContext) => {
     })
     t.assert.strictEqual(check.statusCode, 404, 'employee should not exist anymore')
     t.assert.deepEqual(check.json().message, 'Employee not found')
+})
+
+test("return error (404) if employee not found when deleting", async (t:TestContext) => {
+    t.plan(2)
+    const app = buildApp({
+        logger: false
+    })
+
+    const response = await app.inject({
+        method: 'DELETE',
+        url: '/employee/-1'
+    })
+
+    t.assert.strictEqual(response.statusCode, 404, 'response error')
+    t.assert.deepStrictEqual(response.json(), {
+        message: "Employee not found"
+    })
 })
