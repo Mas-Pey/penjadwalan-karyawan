e4c3df4fd7ce35bfbc17a86e9fdb4eb15bc02987
Mas-Pey <konamexxxpes@gmail.com>
2025-10-10 11:33:04 +0700

    feat: add schedules table

--- src/features/db.ts
+++ src/features/db.ts
@@ -3,10 +3,23 @@ import type { Database as BetterSqliteDatabase } from "better-sqlite3"
 
 const db: BetterSqliteDatabase = new Database('employee.db', { verbose: console.log })
 
+// === TABLE employees ===
 db.prepare(`
     CREATE TABLE IF NOT EXISTS employees (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT NOT NULL 
+    )
+`).run()
+
+// === TABLE schedules ===
+db.prepare(`
+    CREATE TABLE IF NOT EXISTS schedules (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        date TEXT NOT NULL,
+        shift TEXT NOT NULL,
+        start_time TEXT NOT NULL,
+        end_time TEXT NOT NULL,
+        employees TEXT NOT NULL
     )
 `).run()
 
