0f736272161b806895db6b7233b8c866527aa75e
Mas-Pey <konamexxxpes@gmail.com>
2025-10-14 10:59:12 +0700

    feat: add schedule endpoint

---
+++ src/features/schedulee.ts
@@ -0,0 +1,75 @@
+import type { FastifyPluginAsync, FastifyReply, FastifyRequest } from "fastify"
+import fp from 'fastify-plugin'
+import db from './db.ts'
+import employee from "./employee.ts"
+import { start } from "repl"
+
+/**
+ * TYPES 
+ * 
+ */
+interface ScheduleBody {
+    month: number
+    holidays?: string[]
+}
+
+/**
+ * 
+ * ROUTES
+ */
+const scheduleRoutes: FastifyPluginAsync = async (fastify) => {
+    fastify.post<{ Body: ScheduleBody }>('/create-schedule',
+        async (request, reply) => {
+            const { month, holidays = [] } = request.body
+
+            if (typeof month !== "number" || month < 1 || month > 12) {
+                return reply.status(404).send({
+                    message: "The 'month' must be a number between 1 and 12."
+                })
+            }
+
+            // === DATE MANIPULATION ===
+            const year = new Date().getFullYear()
+            const totalDays = new Date(year, month, 0).getDate()
+            const monthPad = String(month).padStart(2, "0");
+
+            const allDates: string[] = []
+            for (let day = 1; day <= totalDays; day++) {
+                const date = `${year}-${monthPad}-${String(day).padStart(2, "0")}`
+                if(!holidays.includes(date)){
+                    allDates.push(date)
+                }
+            }
+
+            // === CHECK EMPLOYEES ===
+            const employees = db.prepare("SELECT name FROM employees").all() as { name: string }[]
+            if (employees.length < 2){
+                return reply.status(400).send({
+                    message: "At least 2 employees are required to generate a schedule.",
+                })
+            }
+
+            // === SHIFT CONFIGURATION ===
+            const shifts = [
+                {
+                    time: "07:00-15:00", 
+                    start_time: "07.00", 
+                    end_time: "15:00", 
+                    hours: 8
+                },
+                {   
+                    time: "15:00-23:00", 
+                    start_time: "15.00", 
+                    end_time: "23:00", 
+                    hours: 8
+                }
+            ]
+
+
+        }
+    )
+}
+
+export default fp(async function (app) {
+    app.register(scheduleRoutes)
+})
