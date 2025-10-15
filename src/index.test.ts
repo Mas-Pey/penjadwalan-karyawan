7620005f8a1bf225dc51662897799fb281b0c632
Mas-Pey <konamexxxpes@gmail.com>
2025-10-15 14:24:18 +0700

    test: add schedule creation tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,10 +1,9 @@
 import { test, type TestContext } from 'node:test'
 import { buildApp } from './server.ts'
-import employee from './features/employee.ts'
 
 let employeeId = ''
 
-
+// ========================= Employee Feature Tests =========================
 test("can create employee", async (t: TestContext) => {
     t.plan(3)
     const app = buildApp({
@@ -42,6 +41,24 @@ test("return error (404) if name is not a string", async (t: TestContext) => {
     t.assert.deepStrictEqual(response.json(), {
         message: "Name must be a text"
     })
+})
+
+test("can return all employees", async (t: TestContext) => {
+    t.plan(3)
+    const app = buildApp({
+        logger: false
+    })
+
+    const response = await app.inject({
+        method: 'GET',
+        url: '/employees',
+    })
+
+    t.assert.strictEqual(response.statusCode, 200, 'response 200')
+    const json = response.json()
+    t.assert.ok(Array.isArray(json.employees), 'employees is an array')
+    t.assert.ok(json.employees.length > 0, 'employees is not empty')
+
 })
 
 test("can return employee info", async (t: TestContext) => {
@@ -135,4 +152,37 @@ test("return error (404) if employee not found when deleting", async (t:TestCont
     t.assert.deepStrictEqual(response.json(), {
         message: "Employee not found"
     })
+})
+
+// ========================= Schedule Feature Tests =========================
+test("POST /create-schedule", async (t: TestContext) => {
+
+    await t.test("can create schedule for a month", async (t: TestContext) => {
+        t.plan(6)
+        const app = buildApp({
+            logger: false
+        })
+
+        const response = await app.inject({
+            method: 'POST',
+            url: '/create-schedule',
+            payload: { month: 10, total_employee: 10 }
+        }) 
+
+        t.assert.strictEqual(response.statusCode, 200, 'response 200')
+        const json = response.json()
+        t.assert.equal(json.schedules.length, 30 * 2, 'there should be 60 shifts in October')
+        t.assert.ok(response.json().summary, 'response has summary')
+
+        const schedule = json.schedules[0]
+        t.assert.ok(schedule.date, 'schedule has date')
+        t.assert.ok(Array.isArray(schedule.employees), 'employees is an array')
+        t.assert.ok(schedule.employees.length > 0, 'employees is not empty')
+        t.assert.ok(schedule.time_start, 'schedule has time_start')
+        t.assert.ok(schedule.time_end, 'schedule has time_end')
+    })
+
+    
+
+
 })
